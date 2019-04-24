1da3195c902cdcc65d70334d2fd3c940770ad508
cohyunoh <coh00@stuy.edu>
2019-04-24 19:35:52 -0400

    started pushUp and accounted for repeating values in my comparators

--- MyHeap.java
+++ MyHeap.java
@@ -3,7 +3,7 @@ public class MyHeap{
   private static void pushDown(int[]data,int size,int index){
     if((2*index + 1) > size - 1 && (2*index + 2) > size - 1){
       return ;
-    }else if(data[(2*index + 1)] < data[index] && data[(2*index + 2)] < data[index]){
+    }else if(data[(2*index + 1)] <= data[index] && data[(2*index + 2)] <= data[index]){
       return ;
     }else if(data[(2*index + 1)] > data[index]){
       int temp = data[(2*index + 1)];
@@ -17,6 +17,17 @@ public class MyHeap{
       data[index] = temp;
       index = 2*index + 2;
       pushDown(data, size, index);
+    }
+  }
+
+  private static void pushUp(int[]data,int index){
+    int parentIndex = ((index - 1) / 2);
+    if(parentIndex < 0){
+      return ;
+    }else if(data[parentIndex] >= data[index]){
+      return ;
+    }else if(data[parentIndex] < data){
+
     }
   }
 
