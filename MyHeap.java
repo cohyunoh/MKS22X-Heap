fceccc62a1f49b51d5e9c7501f175d5a47fe3e0e
cohyunoh <coh00@stuy.edu>
2019-04-24 19:23:20 -0400

    began coding pushdown also realized i didnt need to make instance variables or constructors

--- MyHeap.java
+++ MyHeap.java
@@ -1,4 +1,15 @@
 public class MyHeap{
-  private int[] data;
-  
+  private static void pushDown(int[]data,int size,int index){
+    if((2*index + 1) > size - 1 && (2*index + 2) > size - 1){
+      return ;
+    }else if(data[(2*index + 1)] < data[index] && data[(2*index + 2)] < data[index]){
+      return ;
+    }else if(data[(2*index + 1)] > data[index]){
+      int temp = data[(2*index + 1)];
+      data[(2*index + 1)] = data[index];
+      data[index] = temp;
+      index = (2*index + 1)
+      pushdown(data, size, index;)
+    }else if(data[(2*index + 2)] > data[index])
+  }
 }
